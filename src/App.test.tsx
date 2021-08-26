e9009c10042fe0af2dfbc69f98f98a15d5264e84
AnSc <mksdb@sedna-soft.de>
2021-08-26 15:18:22 +0000

    Create basic test cases for initial view

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {render} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+it('renders a search box with a button', () => {
+    const {getByRole} = render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+    expect(getByRole(/entry/)).toBeInTheDocument();
+    expect(getByRole(/pushbutton/)).toBeInTheDocument();
 });
+it('lists supported features', () => {
+    const {getByRole, getByText} = render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    );
+
+    expect(getByRole(/list/)).toBeInTheDocument();
+    expect(getByText(/unterstützt/)).toBeInTheDocument();
+})
