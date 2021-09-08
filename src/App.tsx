863b652d02456d6300fa99581502ca6f552d5252
AnSc <mksdb@sedna-soft.de>
2021-09-08 13:51:02 +0000

    Improve handling app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import React, {FC, useState} from 'react';
-import {Input, Layout} from "antd";
+import {Card, Input, Layout, PageHeader} from "antd";
 import {ResultList} from "./ResultList";
 import {UsageNotes} from "./UsageNotes";
+import {SearchMatch} from "./service/songApi";
 
 const {Search} = Input;
 
 const App: FC = () => {
-    const [searchString, setSearchString] = useState('');
-    const [selectedId, setSelectedId] = useState<number | undefined>();
+    const [fieldValue, setFieldValue] = useState('');
+    const [searchExpression, setSearchExpression] = useState('');
+    const [selectedMatch, setSelectedMatch] = useState<SearchMatch | undefined>();
     return <Layout>
         <Layout.Content>
-            <Search allowClear onSearch={setSearchString} enterButton/>
-            {searchString
-                ? <ResultList search={searchString} onSelect={(match) => setSelectedId(match.id)}/>
-                : <UsageNotes/>
-            }
-            {selectedId && <pre>{selectedId}</pre>}
+            <PageHeader
+                title={selectedMatch
+                    ? `„${selectedMatch.title}“ ${selectedMatch.copyright_year || ''}`
+                    : 'Suche'
+                }
+                subTitle={selectedMatch ? null : 'in der Schlagerdatenbank'}
+                backIcon={selectedMatch ? undefined : false}
+                onBack={() => setSelectedMatch(undefined)}
+                extra={selectedMatch
+                    ? undefined
+                    : <Search
+                        allowClear
+                        enterButton
+                        onSearch={setSearchExpression}
+                        onChange={(event) => setFieldValue(event.target.value)}
+                        style={{width: '50vw'}}
+                        value={fieldValue}
+                    />
+                }
+            >{searchExpression ? undefined : <UsageNotes/>}</PageHeader>
+            <Card>
+                {selectedMatch
+                    && <pre>{selectedMatch.id}</pre>
+                }
+                <ResultList style={{display: !searchExpression || selectedMatch ? 'none' : ''}} search={searchExpression} onSelect={setSelectedMatch}/>
+            </Card>
         </Layout.Content>
     </Layout>;
 };
