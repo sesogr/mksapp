ce06e78604aed9f3a88fae403645d1f1e7f60da5
AnSc <mksdb@sedna-soft.de>
2021-09-09 06:55:43 +0000

    Remove counter from store config

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import {songApi} from "../service/songApi";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     [songApi.reducerPath]: songApi.reducer,
   },
 });
