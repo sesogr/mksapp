38dacba534d76344ddafd74014a7bdd28b406c29
MFH <maximilian-felix.helm@stud.hszg.de>
2021-09-17 15:30:25 +0200

    add description for advanced search in UsageNotes.tsx (and some small adjustments)

--- src/UsageNotes.tsx
+++ src/UsageNotes.tsx
@@ -7,16 +7,34 @@ const {Paragraph, Text} = Typography;
 type UsageNotesProps = { extended: boolean };
 export const UsageNotes: FC<UsageNotesProps> = ({extended}) => extended
     ? <>
-        <Paragraph>Diese Mehrfeld-Suche sucht nach Stichwörtern in den entsprechenden Feldern.</Paragraph>
+        <Paragraph>Diese Mehrfeld-Suche sucht nach Stichwörtern in den entsprechenden Feldern. Dabei wird (pro Feld) unterstützt:</Paragraph>
+        <ul>
+            <li>
+                Kombinationen einfacher Stichwörter, z. B.
+                <Text keyboard>märchen glück</Text>
+            </li>
+            <li>
+                Phrasen in doppelten Anführungszeichen, z. B.
+                <Text keyboard><Text strong>"am Himmel"</Text> Operette</Text>
+            </li>
+            <li>
+                Ausschluss von Wörtern/Phrasen mit Minus, z. B.
+                <Text keyboard>Frühling <Text strong>-Wien</Text></Text>
+            </li>
+        </ul>
         <Paragraph>
-            Es gibt aber auch eine einzeilige Suche in jeglichen Detailinformationen zu einem Schlager,
+            Sollte(n) ein oder mehrere Ergebnisse alle Suchbegriffe voll erfüllen, werden nur sie angezeigt.
+            Andernfalls werden alle Ergbnisse die mindestens ein Suchbegriff beinhalten angezeigt.
+        </Paragraph>
+        <Paragraph>
+            Es gibt aber auch eine einzeilige Suche, welche in jeglichen Detailinformationen zu einem Schlager sucht,
             die über die Schaltfläche
-            <Text keyboard><EllipsisOutlined/></Text>
+            <Text keyboard>Einzeilige Suche</Text>
             zu erreichen ist.
         </Paragraph>
     </>
     : <>
-        <Text>Diese einzeilige Suche durchsucht jegliche Detailinformationen zu einem Schlager und unterstützt</Text>
+        <Text>Diese einzeilige Suche durchsucht jegliche Detailinformationen zu einem Schlager und unterstützt:</Text>
         <ul>
             <li>
                 Kombinationen einfacher Stichwörter, z. B.
@@ -39,6 +57,10 @@ export const UsageNotes: FC<UsageNotesProps> = ({extended}) => extended
             wenn UND-Kombination der Stichwörter/Phrasen erfolglos
         </li>*/}
         </ul>
+        <Paragraph>
+            Sollte(n) ein oder mehrere Ergebnisse alle Suchbegriffe voll erfüllen, werden nur sie angezeigt.
+            Andernfalls werden alle Ergbnisse die mindestens ein Suchbegriff beinhalten angezeigt.
+        </Paragraph>
         <Paragraph>
             Es gibt aber auch eine Mehrfeld-Suche, die über die Schaltfläche
             <Text keyboard><EllipsisOutlined/></Text>
