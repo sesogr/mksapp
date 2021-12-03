8f8846facd6dccd899bdc996444df8194e5fac5a
AnSc <mksdb@sedna-soft.de>
2021-12-03 16:54:54 +0000

    Localise sorting instruction tooltips
    de_DE is the only German locale available, de_AT wasn't supported

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import 'antd/dist/antd.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import {ConfigProvider} from "antd";
+import deDE from 'antd/lib/locale/de_DE';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <ConfigProvider locale={deDE}>
+          <App />
+        </ConfigProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
